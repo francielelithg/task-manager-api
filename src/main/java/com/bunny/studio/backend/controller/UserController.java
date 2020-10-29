d1dd6576cfc7ef7bb2d59a1be5c29b0d0e14d9a4
Franciele Lithg <francielelithg@gmail.com>
2020-10-29 10:06:04 -0300

    swagger

--- src/main/java/com/bunny/studio/backend/controller/UserController.java
+++ src/main/java/com/bunny/studio/backend/controller/UserController.java
@@ -14,33 +14,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bunny.studio.backend.model.User;
 import com.bunny.studio.backend.repository.UserRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/user")
+@Api(value = "Users")
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "Success"),
+        @ApiResponse(code = 401, message = "Unauthorized"),
+        @ApiResponse(code = 404, message = "User not found"),
+        @ApiResponse(code = 500, message = "Server was unable to process the request")
+})
 public class UserController {
 	
 	@Autowired
 	private UserRepository repo;
 	
 	@GetMapping
+	@ApiOperation(value = "Retrieve all users", response = Iterable.class)
 	public Iterable<User> findAll(){
 	   return repo.findAll();
 	}
 	
 	@SuppressWarnings("rawtypes")
 	@GetMapping(path = {"/{id}"})
-	public ResponseEntity findById(@PathVariable long id){
+	@ApiOperation(value = "Get user by id", response = ResponseEntity.class)
+	public ResponseEntity findById(@ApiParam(value = "Id of the user", required = true) @PathVariable long id){
 	   return repo.findById(id)
 			   .map(record -> ResponseEntity.ok().body(record))
 	           .orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
-	public User create(@RequestBody User usuario){
+	@ApiOperation(value = "Create a new user", response = User.class)
+	public User create(@ApiParam(value = "Information of the user to be created", required = true) @RequestBody User usuario){
 	   return repo.save(usuario);
 	}
 
 	@PutMapping(value="/{id}")
-	public ResponseEntity<User> update(@PathVariable("id") long id, @RequestBody User usuario){
+	@ApiOperation(value = "Update an existing user", response = ResponseEntity.class)
+	public ResponseEntity<User> update(@ApiParam(value = "Id of the user", required = true) @PathVariable("id") long id, @ApiParam(value = "Information of the user to be updated", required = true) @RequestBody User usuario){
 		return repo.findById(id)
 		           .map(record -> {
 		               record.setName(usuario.getName());
@@ -50,7 +67,8 @@ public class UserController {
 	}
 	
 	@DeleteMapping(path ={"/{id}"})
-	public ResponseEntity<?> delete(@PathVariable long id) {
+	@ApiOperation(value = "Delete a user", response = ResponseEntity.class)
+	public ResponseEntity<?> delete(@ApiParam(value = "Id of the user to be deleted", required = true) @PathVariable long id) {
 	   return repo.findById(id)
 	           .map(record -> {
 	               repo.deleteById(id);
--- src/main/java/com/bunny/studio/backend/controller/UserTasksController.java
+++ src/main/java/com/bunny/studio/backend/controller/UserTasksController.java
@@ -14,33 +14,50 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bunny.studio.backend.model.UserTasks;
 import com.bunny.studio.backend.repository.UserTasksRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/userTasks")
+@Api(value = "Tasks")
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "Success"),
+        @ApiResponse(code = 401, message = "Unauthorized"),
+        @ApiResponse(code = 404, message = "Task not found"),
+        @ApiResponse(code = 500, message = "Server was unable to process the request")
+})
 public class UserTasksController {
 	
 	@Autowired
 	private UserTasksRepository repo;
 	
 	@GetMapping
+	@ApiOperation(value = "Retrieve all tasks", response = Iterable.class)
 	public Iterable<UserTasks> findAll(){
 	   return repo.findAll();
 	}
 	
 	@SuppressWarnings("rawtypes")
 	@GetMapping(path = {"/{id}"})
-	public ResponseEntity findById(@PathVariable long id){
+	@ApiOperation(value = "Get task by id", response = ResponseEntity.class)
+	public ResponseEntity findById(@ApiParam(value = "Id of the task", required = true) @PathVariable long id){
 	   return repo.findById(id)
 			   .map(record -> ResponseEntity.ok().body(record))
 	           .orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping
-	public UserTasks create(@RequestBody UserTasks task){
+	@ApiOperation(value = "Create a new task", response = UserTasks.class)
+	public UserTasks create(@ApiParam(value = "Information of the task to be created", required = true) @RequestBody UserTasks task){
 	   return repo.save(task);
 	}
 	
 	@PutMapping(value="/{id}")
-	public ResponseEntity<UserTasks> update(@PathVariable("id") long id, @RequestBody UserTasks task){
+	@ApiOperation(value = "Update an existing task", response = ResponseEntity.class)
+	public ResponseEntity<UserTasks> update(@ApiParam(value = "Id of the task", required = true) @PathVariable("id") long id, @ApiParam(value = "Information of the task to be updated", required = true) @RequestBody UserTasks task){
 		return repo.findById(id)
 		           .map(record -> {
 		               record.setDescription(task.getDescription());
@@ -51,7 +68,8 @@ public class UserTasksController {
 	}
 	
 	@DeleteMapping(path ={"/{id}"})
-	public ResponseEntity<?> delete(@PathVariable long id) {
+	@ApiOperation(value = "Delete a task", response = ResponseEntity.class)
+	public ResponseEntity<?> delete(@ApiParam(value = "Id of the task to be deleted", required = true) @PathVariable long id) {
 	   return repo.findById(id)
 	           .map(record -> {
 	               repo.deleteById(id);
